5583a30b6775d002df6defc034f10a44bb0eb20c
P-Copley <pkcopley@gmail.com>
2021-04-21 11:16:39 +0100

    add test data

---
+++ db/data/test/baskets.js
@@ -0,0 +1,10 @@
+module.exports = [
+  {
+    username: 'Paul-R',
+    item_id: 3,
+  },
+  {
+    username: 'Paul-R',
+    item_id: 4,
+  },
+];
---
+++ db/data/test/users.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    username: 'Paul-R',
+    avatar_url: 'https://test.com/Paul-R.jpg',
+  },
+  {
+    username: 'Paul-C',
+    avatar_url: 'https://test.com/Paul-C.jpg',
+  },
+  {
+    username: 'Izzi',
+    avatar_url: 'https://test.com/Izzi.jpg',
+  },
+  {
+    username: 'Ant',
+    avatar_url: 'https://test.com/Ant.jpg',
+  },
+];
