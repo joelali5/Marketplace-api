798af9caa41511d1a9f98e799b7b9ce5f7374f84
P-Copley <pkcopley@gmail.com>
2021-04-21 10:48:47 +0100

    use username as users primary key

--- db/migrations/20210421091523_create-users-table.js
+++ db/migrations/20210421091523_create-users-table.js
@@ -1,7 +1,6 @@
 exports.up = function (knex) {
   return knex.schema.createTable('users', (usersTable) => {
-    usersTable.increments().primary();
-    usersTable.string('username').notNullable();
+    usersTable.string('username').primary();
     usersTable.string('full_name').notNullable();
     usersTable.text('avatar_url');
     usersTable.integer('kudos').defaultTo(0);
