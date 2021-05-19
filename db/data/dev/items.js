f19724da4fa7c71692da5f902d2dc426af9a40c6
P-Copley <pkcopley@gmail.com>
2021-05-19 13:06:38 +0100

    crop image height as well as width

--- db/data/dev/items.js
+++ db/data/dev/items.js
@@ -3,7 +3,7 @@ module.exports = [
     item_name: '1990s Gameboy',
     description: 'But mom I want one!',
     img_url:
-      'https://images.unsplash.com/photo-1599933190257-ade62d308472?fit=crop&w=600&q=80',
+      'https://images.unsplash.com/photo-1599933190257-ade62d308472?fit=crop&w=600&h=600&q=80',
     price: 1599,
     category_name: 'Electronics',
   },
@@ -11,7 +11,7 @@ module.exports = [
     item_name: 'New iMac',
     description: 'Good lord is it pretty',
     img_url:
-      'https://images.unsplash.com/photo-1515248137880-45e105b710e0?fit=crop&w=600&q=80',
+      'https://images.unsplash.com/photo-1515248137880-45e105b710e0?fit=crop&w=600&h=600&q=80',
     price: 100000000,
     category_name: 'Electronics',
   },
@@ -19,7 +19,7 @@ module.exports = [
     item_name: 'React Native online tutorials',
     description: 'Are React and React Native the same thing though?',
     img_url:
-      'https://images.unsplash.com/photo-1518773553398-650c184e0bb3?fit=crop&w=600&q=80',
+      'https://images.unsplash.com/photo-1518773553398-650c184e0bb3?fit=crop&w=600&h=600&q=80',
     price: 1099,
     category_name: 'Electronics',
   },
@@ -27,7 +27,7 @@ module.exports = [
     item_name: 'Antique Bookshelf',
     description: 'Makes your apartment smell like rich mahogany',
     img_url:
-      'https://images.unsplash.com/photo-1519612632649-158aa883572a?fit=crop&w=600&q=80',
+      'https://images.unsplash.com/photo-1519612632649-158aa883572a?fit=crop&w=600&h=600&q=80',
     price: 7999,
     category_name: 'Household',
   },
@@ -35,7 +35,7 @@ module.exports = [
     item_name: 'Drinks Globe',
     description: 'Booze and geography combined',
     img_url:
-      'https://images.unsplash.com/photo-1553304961-2ec9700cf283?fit=crop&w=600&q=80',
+      'https://images.unsplash.com/photo-1553304961-2ec9700cf283?fit=crop&w=600&h=600&q=80',
     price: 1800,
     category_name: 'Household',
   },
@@ -43,7 +43,7 @@ module.exports = [
     item_name: 'Hand crafted pottery tea cups',
     description: 'Comes with a free homely vibe',
     img_url:
-      'https://images.unsplash.com/photo-1590422749897-47036da0b0ff?fit=crop&w=600&q=80',
+      'https://images.unsplash.com/photo-1590422749897-47036da0b0ff?fit=crop&w=600&h=600&q=80',
     price: 3000,
     category_name: 'Household',
   },
@@ -51,7 +51,7 @@ module.exports = [
     item_name: "Big 'ol pile of beanies",
     description: 'Comfy and warm!',
     img_url:
-      'https://images.unsplash.com/photo-1574368827145-a1e35c5cc329?fit=crop&w=600&q=80',
+      'https://images.unsplash.com/photo-1574368827145-a1e35c5cc329?fit=crop&w=600&h=600&q=80',
     price: 1200,
     category_name: 'Clothing',
   },
@@ -60,7 +60,7 @@ module.exports = [
     description:
       "Let the world know about all the cool tech you're into... or just fill it with random nonsense, you do you :D ",
     img_url:
-      'https://images.unsplash.com/photo-1589384267710-7a170981ca78?fit=crop&w=600&q=80',
+      'https://images.unsplash.com/photo-1589384267710-7a170981ca78?fit=crop&w=600&h=600&q=80',
     price: 600,
     category_name: 'Electronics',
   },
@@ -68,7 +68,7 @@ module.exports = [
     item_name: 'Headphones',
     description: 'The coding equivalent of a do not disturb sign',
     img_url:
-      'https://images.unsplash.com/photo-1528575950036-63c4853d3f6f?fit=crop&w=600&q=80',
+      'https://images.unsplash.com/photo-1528575950036-63c4853d3f6f?fit=crop&w=600&h=600&q=80',
     price: 3000,
     category_name: 'Electronics',
   },
@@ -76,7 +76,7 @@ module.exports = [
     item_name: 'Plain White T',
     description: 'A modern classic',
     img_url:
-      'https://images.unsplash.com/photo-1581655353564-df123a1eb820?fit=crop&w=600&q=80',
+      'https://images.unsplash.com/photo-1581655353564-df123a1eb820?fit=crop&w=600&h=600&q=80',
     price: 799,
     category_name: 'Clothing',
   },
@@ -85,7 +85,7 @@ module.exports = [
     description:
       'Can be used to achieve literally anything. Case in point - a gravity defying banana.',
     img_url:
-      'https://images.unsplash.com/photo-1575846250428-cffcf571b04e?fit=crop&w=600&q=80',
+      'https://images.unsplash.com/photo-1575846250428-cffcf571b04e?fit=crop&w=600&h=600&q=80',
     price: 899,
     category_name: 'Household',
   },
@@ -93,7 +93,7 @@ module.exports = [
     item_name: 'Chess Board',
     description: 'You could be the next Magnus Carlsen',
     img_url:
-      'https://images.unsplash.com/photo-1596507457972-0ab33221e1cf?fit=crop&w=600&q=80',
+      'https://images.unsplash.com/photo-1596507457972-0ab33221e1cf?fit=crop&w=600&h=600&q=80',
     price: 2500,
     category_name: 'Household',
   },
@@ -101,7 +101,7 @@ module.exports = [
     item_name: 'Surf Board',
     description: 'Surfs up dude',
     img_url:
-      'https://images.unsplash.com/photo-1528150177508-7cc0c36cda5c?fit=crop&w=600&q=80',
+      'https://images.unsplash.com/photo-1528150177508-7cc0c36cda5c?fit=crop&w=600&h=600&q=80',
     price: 4000,
     category_name: 'Household',
   },
@@ -109,7 +109,7 @@ module.exports = [
     item_name: 'Satchel',
     description: 'Perfectly laptop sized',
     img_url:
-      'https://images.unsplash.com/photo-1484606067694-f2f9b209a225?fit=crop&w=600&q=80',
+      'https://images.unsplash.com/photo-1484606067694-f2f9b209a225?fit=crop&w=600&h=600&q=80',
     price: 1800,
     category_name: 'Clothing',
   },
@@ -117,7 +117,7 @@ module.exports = [
     item_name: 'Watch',
     description: 'What time is it? Time you got a watch',
     img_url:
-      'https://images.unsplash.com/photo-1518131672697-613becd4fab5?fit=crop&w=600&q=80',
+      'https://images.unsplash.com/photo-1518131672697-613becd4fab5?fit=crop&w=600&h=600&q=80',
     price: 7000,
     category_name: 'Clothing',
   },
@@ -125,7 +125,7 @@ module.exports = [
     item_name: 'Pocket Watch',
     description: 'What time is it? Time you got a watch',
     img_url:
-      'https://images.unsplash.com/photo-1509048191080-d2984bad6ae5?fit=crop&w=600&q=80',
+      'https://images.unsplash.com/photo-1509048191080-d2984bad6ae5?fit=crop&w=600&h=600&q=80',
     price: 8000,
     category_name: 'Clothing',
   },
@@ -133,7 +133,7 @@ module.exports = [
     item_name: 'Watercolour paint set',
     description: 'Bob ross would be proud',
     img_url:
-      'https://images.unsplash.com/photo-1550001447-f78ba21d9390?fit=crop&w=600&q=80',
+      'https://images.unsplash.com/photo-1550001447-f78ba21d9390?fit=crop&w=600&h=600&q=80',
     price: 2000,
     category_name: 'Household',
   },
@@ -141,7 +141,7 @@ module.exports = [
     item_name: 'Electric guitar',
     description: 'This one goes up to eleven',
     img_url:
-      'https://images.unsplash.com/photo-1592922823354-c48e6d894bbe?fit=crop&w=600&q=80',
+      'https://images.unsplash.com/photo-1592922823354-c48e6d894bbe?fit=crop&w=600&h=600&q=80',
     price: 2000,
     category_name: 'Electronics',
   },
@@ -150,7 +150,7 @@ module.exports = [
     description:
       'Get some jeans that have seen it all without having to go through all those awkward life experiences yourself',
     img_url:
-      'https://images.unsplash.com/photo-1541099649105-f69ad21f3246?fit=crop&w=600&q=80',
+      'https://images.unsplash.com/photo-1541099649105-f69ad21f3246?fit=crop&w=600&h=600&q=80',
     price: 1700,
     category_name: 'Clothing',
   },
@@ -158,7 +158,7 @@ module.exports = [
     item_name: 'Raincoat',
     description: 'Perfect for exploring the great outdoors',
     img_url:
-      'https://images.unsplash.com/photo-1537045439788-4e69f886651a?fit=crop&w=600&q=80',
+      'https://images.unsplash.com/photo-1537045439788-4e69f886651a?fit=crop&w=600&h=600&q=80',
     price: 3999,
     category_name: 'Clothing',
   },
@@ -166,7 +166,7 @@ module.exports = [
     item_name: 'Light up suit',
     description: 'Ideal for raves',
     img_url:
-      'https://images.unsplash.com/photo-1509817445409-e2057fd6feac?fit=crop&w=600&q=80',
+      'https://images.unsplash.com/photo-1509817445409-e2057fd6feac?fit=crop&w=600&h=600&q=80',
     price: 10000,
     category_name: 'Clothing',
   },
@@ -175,7 +175,7 @@ module.exports = [
     description:
       "Think of all the cool aerial fly overs you'll be able to take",
     img_url:
-      'https://images.unsplash.com/photo-1591105575839-1fb30d5ce4a5?fit=crop&w=600&q=80',
+      'https://images.unsplash.com/photo-1591105575839-1fb30d5ce4a5?fit=crop&w=600&h=600&q=80',
     price: 6000,
     category_name: 'Electronics',
   },
@@ -183,7 +183,7 @@ module.exports = [
     item_name: 'See through desktop case',
     description: 'Cat not included',
     img_url:
-      'https://images.unsplash.com/photo-1607034071833-18a5b64047ac?fit=crop&w=600&q=80',
+      'https://images.unsplash.com/photo-1607034071833-18a5b64047ac?fit=crop&w=600&h=600&q=80',
     price: 2000,
     category_name: 'Electronics',
   },
@@ -191,7 +191,7 @@ module.exports = [
     item_name: 'Desktop Microphone',
     description: 'Crystal clear quality on all your zoom calls',
     img_url:
-      'https://images.unsplash.com/photo-1583593711082-aaa381feb2f1?fit=crop&w=600&q=80',
+      'https://images.unsplash.com/photo-1583593711082-aaa381feb2f1?fit=crop&w=600&h=600&q=80',
     price: 9000,
     category_name: 'Electronics',
   },
@@ -199,7 +199,7 @@ module.exports = [
     item_name: 'Ultra-wide Monitor',
     description: 'Capable of opening over 200 browser tabs simultaneously',
     img_url:
-      'https://images.unsplash.com/photo-1606127195437-d12564b184e9?fit=crop&w=600&q=80',
+      'https://images.unsplash.com/photo-1606127195437-d12564b184e9?fit=crop&w=600&h=600&q=80',
     price: 30000,
     category_name: 'Electronics',
   },
@@ -208,7 +208,7 @@ module.exports = [
     description:
       'As made famous by terrible handwriting in northcoders lectures',
     img_url:
-      'https://images.unsplash.com/photo-1569323112693-492fe7ec1d3b?fit=crop&w=600&q=80',
+      'https://images.unsplash.com/photo-1569323112693-492fe7ec1d3b?fit=crop&w=600&h=600&q=80',
     price: 4000,
     category_name: 'Electronics',
   },
@@ -216,7 +216,7 @@ module.exports = [
     item_name: 'Vintage Piano',
     description: "Even if you can't play it sure does look cool",
     img_url:
-      'https://images.unsplash.com/photo-1560105358-02361fa27b2c?fit=crop&w=600&q=80',
+      'https://images.unsplash.com/photo-1560105358-02361fa27b2c?fit=crop&w=600&h=600&q=80',
     price: 200000,
     category_name: 'Household',
   },
@@ -224,7 +224,7 @@ module.exports = [
     item_name: 'Standing desk',
     description: 'Great for your posture whilst coding all day',
     img_url:
-      'https://images.unsplash.com/photo-1599502452965-df0824971bb2?fit=crop&w=600&q=80',
+      'https://images.unsplash.com/photo-1599502452965-df0824971bb2?fit=crop&w=600&h=600&q=80',
     price: 5000,
     category_name: 'Household',
   },
@@ -232,7 +232,7 @@ module.exports = [
     item_name: 'Wicker Chair',
     description: 'Cat not included',
     img_url:
-      'https://images.unsplash.com/photo-1607165993340-633148469d1f?fit=crop&w=600&q=80',
+      'https://images.unsplash.com/photo-1607165993340-633148469d1f?fit=crop&w=600&h=600&q=80',
     price: 2000,
     category_name: 'Household',
   },
@@ -240,7 +240,7 @@ module.exports = [
     item_name: 'Cactus',
     description: 'Spiky',
     img_url:
-      'https://images.unsplash.com/photo-1521503862198-2ae9a997bbc9?fit=crop&w=600&q=80',
+      'https://images.unsplash.com/photo-1521503862198-2ae9a997bbc9?fit=crop&w=600&h=600&q=80',
     price: 899,
     category_name: 'Household',
   },
@@ -248,7 +248,7 @@ module.exports = [
     item_name: 'Wooden Chair',
     description: 'A real vintage feel',
     img_url:
-      'https://images.unsplash.com/photo-1602592867152-ba321a437ff0?fit=crop&w=600&q=80',
+      'https://images.unsplash.com/photo-1602592867152-ba321a437ff0?fit=crop&w=600&h=600&q=80',
     price: 2800,
     category_name: 'Household',
   },
@@ -256,7 +256,7 @@ module.exports = [
     item_name: 'Cat scratching post',
     description: 'Give them something to scratch instead of your furniture',
     img_url:
-      'https://images.unsplash.com/photo-1545249390-6bdfa286032f?fit=crop&w=600&q=80',
+      'https://images.unsplash.com/photo-1545249390-6bdfa286032f?fit=crop&w=600&h=600&q=80',
     price: 1599,
     category_name: 'Household',
   },
@@ -265,11 +265,11 @@ module.exports = [
     description:
       'Express yourself as an individual by doing the same as everyone else',
     img_url:
-      'https://images.unsplash.com/photo-1494496195158-c3becb4f2475?fit=crop&w=600&q=80',
+      'https://images.unsplash.com/photo-1494496195158-c3becb4f2475?fit=crop&w=600&h=600&q=80',
     price: 4000,
     category_name: 'Clothing',
   },
 ];
 
 // this query crops unsplash images to 600px to keep the image file size down
-// &fit=crop&w=600&q=80
+// &fit=crop&w=600&h=600&q=80
