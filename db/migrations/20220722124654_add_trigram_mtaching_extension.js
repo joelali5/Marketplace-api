de8eb86a8a3edcbd342053a195b445038551f893
Katherine <hurstkatherine@hotmail.co.uk>
2022-11-10 15:44:14 +0000

    update files to work with ElephantSQL

--- db/migrations/20220722124654_add_trigram_mtaching_extension.js
+++ db/migrations/20220722124654_add_trigram_mtaching_extension.js
@@ -1,7 +1,7 @@
 exports.up = function (knex) {
-  return knex.raw('CREATE EXTENSION pg_trgm');
+  return knex.raw("CREATE EXTENSION IF NOT EXISTS pg_trgm");
 };
 
 exports.down = function (knex) {
-  return knex.raw('DROP EXTENSION pg_trgm');
+  return Promise.resolve();
 };
